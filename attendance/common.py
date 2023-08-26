3ce234f01c65fa2ab8db1387aa416f61fc9d9fda
kwesy <onesimusgravessampson1@gmail.com>
2023-08-26 23:43:46 +0000

    fix cors-header

--- attendance/common.py
+++ attendance/common.py
@@ -128,10 +128,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    '*'
-]
-CSRF_TRUSTED_ORIGINS = [
-    '*',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://*.com',
+#     'https://*.com',
+
+# ]
+# CSRF_TRUSTED_ORIGINS = [
+#     'http://*.com',
+#     'https://*.com',
+# ]
